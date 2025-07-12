9ade0df5dd7ae0c6c7d46f767ab77bac28c2f14c
henriqueguanais <henriquecorneaudr@gmail.com>
2025-07-12 16:06:00 -0300

    Inclusao da lib

---
+++ rtos-basic/rtos-basic.c
@@ -0,0 +1,40 @@
+#include "pico/stdlib.h"
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+#include <stdio.h>
+
+#define led_pin_red 12
+
+void vBlinkTask()
+{
+
+    for (;;)
+    {
+
+        gpio_put(led_pin_red, 1);
+
+        vTaskDelay(pdMS_TO_TICKS(50));
+
+        gpio_put(led_pin_red, 0);
+
+        vTaskDelay(pdMS_TO_TICKS(950));
+
+        printf("Blinking\n");
+    }
+}
+
+void main()
+{
+
+    stdio_init_all();
+
+    gpio_init(led_pin_red);
+
+    gpio_set_dir(led_pin_red, GPIO_OUT);
+
+    xTaskCreate(vBlinkTask, "Blink Task", 128, NULL, 1, NULL);
+
+    vTaskStartScheduler();
+}
